334ee2880bed6d3e04620d4820a878c4fabf65c9
Felipe Sebben <felipesebben@yahoo.com.br>
2023-12-27 16:41:36 -0300

    feat: adicionado classe que valida, envia e baixa dados de bucket AWS.~

--- backend/datasource/api.py
+++ backend/datasource/api.py
@@ -1,22 +1,40 @@
+import datetime as dt
+from io import BytesIO
 from typing import List
 
+import pandas as pd
+import pyarrow.parquet as pq
 import requests
 from contracts.schema import CompraSchema, GenericSchema
 
 
 class APICollector:
-    def __init__(self, schema):
+    def __init__(self, schema, aws):
         self._schema = schema
-        self._aws = None
+        self._aws = aws
         self._buffer = None
         return
 
     def start(self, param):
+        """
+        Inicia a coleta de dados.
+        """
         response = self.getData(param)
         response = self.extractData(response)
-        return response
+        response = self.transformDf(response)
+        response = self.convertToParquet(response)
+
+        if self._buffer is not None:
+            file_name = self.fileName()
+            print(file_name)
+            self._aws.upload_file(response, file_name)
+            return True
+        return False
 
     def getData(self, param):
+        """
+        Coleta os dados da API.
+        """
         response = None
         if param > 1:
             response = requests.get(
@@ -29,6 +47,9 @@ class APICollector:
         return response
 
     def extractData(self, response):
+        """
+        Extrai os dados do resultado.
+        """
         result: List[GenericSchema] = []
         for item in response:
             index = {}
@@ -40,5 +61,29 @@ class APICollector:
             result.append(index)
         return result
 
-    def transformDf(self):
-        return
+    def transformDf(self, response):
+        """
+        Transforma o resultado em um DataFrame.
+        """
+        result = pd.DataFrame(response)
+        return result
+
+    def convertToParquet(self, response):
+        """
+        Converte o DataFrame em um arquivo parquet.
+        """
+        self._buffer = BytesIO()
+        try:
+            response.to_parquet(self._buffer)
+            return self._buffer
+        except Exception as e:
+            print(f"Erro ao converter o DataFrame para parquet: {e}")
+            self._buffer = None
+
+    def fileName(self):
+        """
+        Formata nome do arquivo conforme fonte e data da coleta.
+        """
+        data_atual = dt.datetime.now().isoformat()
+        match = data_atual.split(".")
+        return f"api/api-response-compra{match[0]}.parquet"
--- backend/start.py
+++ backend/start.py
@@ -1,9 +1,25 @@
+import time
+
+import schedule
+from aws.client import S3Client
 from contracts.schema import CompraSchema, GenericSchema
 from datasource.api import APICollector
 
 schema = CompraSchema
+aws = S3Client()
 
-minha_classe = APICollector(schema).start(1)
+
+def api_collector(schema, aws, repeat: int):
+    """
+    Chama a classe APICollector para coletar os dados.
+    """
+    response = APICollector(schema, aws).start(repeat)
+    print("Executei a chamada da API.")
+    return
 
 
-print(minha_classe)
+schedule.every(1).minutes.do(api_collector, schema, aws, 5)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
